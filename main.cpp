acaf18f6293f74eef427765972e0da2efdd90613
Bob (NesterovAA) <alice@example.com>
2022-10-05 23:56:38 +0300

    s&r&p&d

--- main.cpp
+++ main.cpp
@@ -8,7 +8,8 @@ int main()
     cin >> a >> b;
     cout << "A + B = " << a + b << '\n'
         << "A - B = " << a - b << '\n'
-        << "A * B = " << a * b << '\n';
+        << "A * B = " << a * b << '\n'
+        << "A / B = " << a / b << '\n';
 
     return 0;
 }
