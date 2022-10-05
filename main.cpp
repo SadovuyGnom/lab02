8c0edacbc18999251894e1f286ae86bda52dc34f
Alice (NesterovAA) <alice@example.com>
2022-10-06 00:37:49 +0300

    min

--- main.cpp
+++ main.cpp
@@ -10,6 +10,10 @@ int main()
         << "A - B = " << a - b << '\n'
         << "A * B = " << a * b << '\n'
         << "A / B = " << a / b << '\n';
+      if (a >= b) cout << a << '\n';
+    else cout << b << '\n';
+    if (a <= b) cout << a;
+    else cout << b;
 
     return 0;
 }
